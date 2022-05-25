ce6fd8e5c8b1c2d1f7e46ea09fd457168fdad614
Strages <m.vad95@gmail.com>
2022-05-25 10:50:30 +0300

    feat: add CRUD for types, brands, devices

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
@@ -12,6 +14,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use(errorHandler)
 
 const start = async () => {
