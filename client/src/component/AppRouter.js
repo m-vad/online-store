bc7a6340e709dfbb9ffb50ceb684e320dd941998
Strages <m.vad95@gmail.com>
2022-05-31 21:46:55 +0300

    feat: add device store, user store and navbar

--- client/src/component/AppRouter.js
+++ client/src/component/AppRouter.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom'
 import {authRoutes, publicRoutes} from "../routes";
 import {SHOP_ROUTE} from "../utils/consts";
+import {Context} from "../index";
+import {observer} from "mobx-react-lite";
 
-const AppRouter = () => {
-    const isAuth = true
+const AppRouter = observer(() => {
+    const {user} = useContext(Context)
+
+    console.log(user)
     return (
         <Switch>
-            {isAuth && authRoutes.map(({path, Component}) =>
+            {user.isAuth && authRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} component={Component} exact/>
             )}
             {publicRoutes.map(({path, Component}) =>
@@ -16,6 +20,6 @@ const AppRouter = () => {
             <Redirect to={SHOP_ROUTE}/>
         </Switch>
     );
-};
+});
 
 export default AppRouter;
