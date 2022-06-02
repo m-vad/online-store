4966cd4f5d48db421fffa9a9697f392d2a11b6b1
Strages <m.vad95@gmail.com>
2022-06-02 09:19:13 +0300

    feat: add typebar

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Container} from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {observer} from "mobx-react-lite";
+import TypeBar from "../component/TypeBar";
 
-const Shop = () => {
+const Shop = observer(() => {
     return (
-        <div>
-            SHOP
-        </div>
+        <Container>
+            <Row className="mt-2">
+                <Col md={3}>
+                    <TypeBar/>
+                </Col>
+                <Col md={9}>
+
+                </Col>
+            </Row>
+        </Container>
     );
-};
+});
 
 export default Shop;
