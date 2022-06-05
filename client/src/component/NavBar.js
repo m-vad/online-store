02caec86eae8ea105e1101b8ac42873e58d88115
Strages <m.vad95@gmail.com>
2022-06-05 13:21:39 +0300

    refactor: change navbar

--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -3,26 +3,31 @@ import {Context} from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {Button} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
 import Container from "react-bootstrap/Container";
+import {useHistory} from 'react-router-dom'
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const history = useHistory()
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>Online-store</NavLink>
+                <NavLink style={{color:'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto" style={{color: 'white'}}>
                         <Button
                             variant={"outline-light"}
+                            onClick={() => history.push(ADMIN_ROUTE)}
                         >
                             Админ панель
                         </Button>
                         <Button
                             variant={"outline-light"}
+                            onClick={() => history.push(LOGIN_ROUTE)}
                             className="ml-2"
                         >
                             Выйти
@@ -30,7 +35,7 @@ const NavBar = observer(() => {
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={"outline-light"} onClick={() => user.setIsAuth}>Авторизация</Button>
+                        <Button variant={"outline-light"} onClick={() => history.push(LOGIN_ROUTE)}>Авторизация</Button>
                     </Nav>
                 }
             </Container>
